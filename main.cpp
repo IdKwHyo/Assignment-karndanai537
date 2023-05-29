4a31bdf63479b52accb8ed85e6d2f14cdfe3961f
IdKwHyo <87351345+IdKwHyo@users.noreply.github.com>
2023-05-29 13:47:14 +0000

    redo 1

--- main.cpp
+++ main.cpp
@@ -1,40 +1,35 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
-//1. Creating make file
-//2. Swap the value in the array using pointers** to help
-//3. Take input from argv
 
-int main(int argc, char **argv){
-int i,n,*a;
-n=argc-1;
-a=new int[n];
-for(i=0;i<n;i++) 
-{
-	a[i]=atoi(argv[i+1]);
-	cout<< "a[" << i << "]=" <<a[i]<<endl;
-		}
-
-    int*pa=a;
-    int*pb=a+n-1;
-    int temp;
-    for (i = 0; i < n / 2; i++) {
-     temp = *pa; 
-     *pa = *pb; 
-     *pb = temp;
-    pa++; pb--;
-}
-
-   pa-=n/2;
+int main(int argc, char **argv) {
+  int n, *a;
+  n = argc - 1;
+  a = new int[n];  
   
-     // Using p within your program 
-  for(i=0;i<n;i++) {
- 		   cout<<pa[i]<<" "; //p is the location where p points to
- //     p++;  
- }
+  for (int i = 0; i < n; i++) {
+      a[i] = atoi(argv[i + 1]);
+      cout << "a[" << i << "]=" << a[i] << endl;
+  }
 
- delete [] pa;  
+  int *p = a;
+  int *pb = a + n - 1;
+  int temp;
+  for (int i = 0; i < n / 2; i++) {
+      temp = *p;
+      *p = *pb;
+      *pb = temp;
+      p++;
+      pb--;
+  }
+
+    p = a;
+
+    cout << "Reversed " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "a[" << i << "]=" << a[i] << endl;
+    }
+
+    delete[] a;
+    return 0;
 }
-
-  
-//}
-
